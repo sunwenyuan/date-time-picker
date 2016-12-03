8852084973d86aad4f1395a4dd03b460cb615c6f
Wenyuan Sun <wenyuan.sun1982@gmail.com>
2016-12-03 17:11:40 +0100

    draft positioning

--- src/ui/application/date-picker/date-picker.component.ts
+++ src/ui/application/date-picker/date-picker.component.ts
@@ -1,3 +1,6 @@
+import { EventTargetLike } from 'rxjs/observable/FromEventObservable';
+import { document, EventTarget } from '@angular/platform-browser/src/facade/browser';
+import * as console from 'console';
 import { start } from 'repl';
 import { Component, OnInit } from '@angular/core';
 import * as moment from 'moment';
@@ -19,7 +22,24 @@ export class DatePickerComponent implements OnInit {
     'second': 0,
     'millisecond': 0
   });
+  selectedDate: moment.Moment = moment().set({
+    'hour': 0,
+    'minute': 0,
+    'second': 0,
+    'millisecond': 0
+  });
   dateDisplay: string = '';
+  calendarPosition: { top: number; left: number; right: number; bottom: number; } = {
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0
+  };
+  private calendarSize: { width: number; height: number; } = {
+    width: 336,
+    height: 432
+  };
+  triggerDivId: string = `j17s-calendar-${moment().valueOf()}`;
 
   constructor() { }
 
@@ -29,7 +49,45 @@ export class DatePickerComponent implements OnInit {
   }
 
   inputFieldClickHandler() {
+    this.updateCalendarPosition();
     this.showDatePicker = true;
+  }
+
+  updateCalendarPosition() {
+    const triggerDivElement: HTMLElement = document.getElementById(this.triggerDivId);
+    const { height, top, left, width } = triggerDivElement.getBoundingClientRect();
+    const windowWidth: number = window.innerWidth;
+    const windowHeight: number = window.innerHeight;
+    const right = windowWidth - width - left;
+    const bottom = windowHeight - height - top;
+    if (bottom >= this.calendarSize.height) {
+      if (right + width > this.calendarSize.width) {
+        this.calendarPosition.top = top + height;
+        this.calendarPosition.left = left;
+        this.calendarPosition.bottom = 0;
+        this.calendarPosition.right = 0;
+      }
+      else {
+        this.calendarPosition.top = top + height;
+        this.calendarPosition.right = right;
+        this.calendarPosition.bottom = 0;
+        this.calendarPosition.left = 0;
+      }
+    }
+    else {
+      if (right + width > this.calendarSize.width) {
+        this.calendarPosition.bottom = bottom + height;
+        this.calendarPosition.left = left;
+        this.calendarPosition.right = 0;
+        this.calendarPosition.top = 0;
+      }
+      else {
+        this.calendarPosition.bottom = bottom + height;
+        this.calendarPosition.right = right;
+        this.calendarPosition.left = left;
+        this.calendarPosition.top = 0;
+      }
+    }
   }
 
   refreshDays() {
