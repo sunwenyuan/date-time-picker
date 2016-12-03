f6b3b6cb0023417997d8fcd750880ef0fd021f97
Wenyuan Sun <wenyuan.sun1982@gmail.com>
2016-12-03 20:40:52 +0100

    add style and select function

--- src/ui/application/date-picker/date-picker.component.ts
+++ src/ui/application/date-picker/date-picker.component.ts
@@ -1,8 +1,4 @@
-import { EventTargetLike } from 'rxjs/observable/FromEventObservable';
-import { document, EventTarget } from '@angular/platform-browser/src/facade/browser';
-import * as console from 'console';
-import { start } from 'repl';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Renderer, ElementRef } from '@angular/core';
 import * as moment from 'moment';
 
 @Component({
@@ -11,23 +7,15 @@ import * as moment from 'moment';
   styleUrls: ['./date-picker.component.less']
 })
 export class DatePickerComponent implements OnInit {
+  @Input() dateFormat: string = 'YYYY-MM-DD';
+
   previousMonthBtnText: string = '<';
   nextMonthBtnText: string = '>';
   headerList: string[] = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
   days: string[] = [];
   showDatePicker: boolean = false;
-  currentDate: moment.Moment = moment().set({
-    'hour': 0,
-    'minute': 0,
-    'second': 0,
-    'millisecond': 0
-  });
-  selectedDate: moment.Moment = moment().set({
-    'hour': 0,
-    'minute': 0,
-    'second': 0,
-    'millisecond': 0
-  });
+  currentDate: moment.Moment;
+  selectedDate: moment.Moment;
   dateDisplay: string = '';
   calendarPosition: { top: number; left: number; right: number; bottom: number; } = {
     top: 0,
@@ -39,53 +27,90 @@ export class DatePickerComponent implements OnInit {
     width: 336,
     height: 432
   };
-  triggerDivId: string = `j17s-calendar-${moment().valueOf()}`;
+  triggerDivId: string;
+  clickListener: Function;
 
-  constructor() { }
+  constructor(
+    private renderer: Renderer,
+    private elementRef: ElementRef
+  ) {
+    this.currentDate = moment().set({
+      'hour': 0,
+      'minute': 0,
+      'second': 0,
+      'millisecond': 0
+    });
+    this.selectedDate = moment().set({
+      'hour': 0,
+      'minute': 0,
+      'second': 0,
+      'millisecond': 0
+    });
+    this.triggerDivId = `j17s-calendar-${moment().valueOf()}`;
+
+    this.clickListener = renderer.listenGlobal(
+      'document',
+      'click',
+      (event: MouseEvent) => this.handleGlobalClick(event)
+    );
+  }
 
   ngOnInit() {
-    this.dateDisplay = this.currentDate.format('YYYY-MM-DD');
+    this.dateDisplay = this.currentDate.format(this.dateFormat);
     this.refreshDays();
+  }
+
+  handleGlobalClick(event: MouseEvent) {
+    const withinElement = this.elementRef.nativeElement.contains(event.target);
+    if (!this.elementRef.nativeElement.contains(event.target)) {
+      this.closeCalendar();
+    }
   }
 
   inputFieldClickHandler() {
     this.updateCalendarPosition();
+    this.showCalendar();
+  }
+
+  showCalendar() {
     this.showDatePicker = true;
+  }
+
+  closeCalendar() {
+    this.showDatePicker = false;
   }
 
   updateCalendarPosition() {
     const triggerDivElement: HTMLElement = document.getElementById(this.triggerDivId);
     const { height, top, left, width } = triggerDivElement.getBoundingClientRect();
-    const windowWidth: number = window.innerWidth;
-    const windowHeight: number = window.innerHeight;
-    const right = windowWidth - width - left;
-    const bottom = windowHeight - height - top;
+    const right = window.innerWidth - width - left;
+    const bottom = window.innerHeight - height - top;
+
+    this.calendarPosition = {
+      top: 0,
+      left: 0,
+      right: 0,
+      bottom: 0
+    };
+
     if (bottom >= this.calendarSize.height) {
       if (right + width > this.calendarSize.width) {
         this.calendarPosition.top = top + height;
         this.calendarPosition.left = left;
-        this.calendarPosition.bottom = 0;
-        this.calendarPosition.right = 0;
       }
       else {
         this.calendarPosition.top = top + height;
         this.calendarPosition.right = right;
-        this.calendarPosition.bottom = 0;
-        this.calendarPosition.left = 0;
       }
     }
     else {
       if (right + width > this.calendarSize.width) {
         this.calendarPosition.bottom = bottom + height;
         this.calendarPosition.left = left;
-        this.calendarPosition.right = 0;
-        this.calendarPosition.top = 0;
       }
       else {
         this.calendarPosition.bottom = bottom + height;
         this.calendarPosition.right = right;
-        this.calendarPosition.left = left;
-        this.calendarPosition.top = 0;
       }
     }
   }
@@ -104,6 +129,7 @@ export class DatePickerComponent implements OnInit {
       millisecond: 0
     });
     const dayOfWeek = startDate.day();
+    const totalNumber: number = 42;
     this.days = [];
     for (let index = 0; index < dayOfWeek; index++) {
       this.days.push('');
@@ -111,6 +137,9 @@ export class DatePickerComponent implements OnInit {
     const lastDayOfMonth = startDate.add(1, 'months').subtract(1, 'days').date();
     for (let index = 1; index <= lastDayOfMonth; index++) {
       this.days.push(index.toString());
+    }
+    while (this.days.length < totalNumber) {
+      this.days.push('');
     }
   }
 
@@ -125,6 +154,23 @@ export class DatePickerComponent implements OnInit {
   }
 
   closeBtnHandler() {
-    this.showDatePicker = false;
+    this.closeCalendar();
+  }
+
+  selectDate(day: string) {
+    const year: number = this.currentDate.year();
+    const month: number = this.currentDate.month();
+    this.selectedDate = moment({
+      year,
+      month,
+      day: parseInt(day, 10)
+    });
+  }
+
+  getDayDivCls(day: string) {
+    const selected = this.selectedDate.year() === this.currentDate.year()
+      && this.selectedDate.month() === this.currentDate.month()
+      && this.selectedDate.date().toString() === day;
+    return selected ? 'selected' : '';
   }
 }
